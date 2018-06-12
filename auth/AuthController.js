7ae0bef94ea5c201bf0bf86ba105e0fe68e3e815
小胡 <xiaohu@xiaohudeiMac.local>
2018-06-12 16:38:50 +0800

    part two done

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -8,6 +8,7 @@ var User = require("../user/User");
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 var config = require("../config");
+var VerifyToken = require("./VerifyToken");
 
 router.post("/register", (req, res) => {
   var hashedPassword = bcrypt.hashSync(req.body.password, 8);
@@ -34,19 +35,36 @@ router.post("/register", (req, res) => {
   );
 });
 
-router.get("/me", (req, res) => {
-  var token = req.headers["x-access-token"];
-  if (!token)
-    return res.status(401).send({ auth: false, message: "No token provided." });
-
-  jwt.verify(token, config.secret, (err, decoded) => {
+router.get("/me", VerifyToken, function(req, res, next) {
+  User.findById(req.userId, { password: 0 }, function(err, user) {
     if (err)
-      return res
-        .status(500)
-        .send({ auth: false, message: "Failed to authenticate token." });
+      return res.status(500).send("There was a problem finding the user.");
+    if (!user) return res.status(404).send("No user found.");
 
-    res.status(200).send(decoded);
+    res.status(200).send(user);
   });
+});
+
+
+router.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) return res.status(500).send("Error on the server.");
+    if (!user) return res.status(404).send("No user found.");
+
+    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordIsValid)
+      return res.status(401).send({ auth: false, token: null });
+
+    var token = jwt.sign({ id: user._id }, config.secret, {
+      expiresIn: 86400
+    });
+
+    res.status(200).send({ auth: true, token: token });
+  });
+});
+
+router.get("/logout", (req, res) => {
+  res.status(200).send({ auth: false, token: null });
 });
 
 module.exports = router;
