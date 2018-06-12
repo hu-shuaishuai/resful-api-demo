e2e4539f16068f9b7cf2e861b517cf7b6d4090fe
小胡 <xiaohu@xiaohudeiMac.local>
2018-06-12 11:29:25 +0800

    register me

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ var db = require("./db");
 var UserController = require("./user/UserController");
 app.use("/users", UserController);
 
+var AuthCOntroller = require('./auth/AuthController');
+app.use('/api/auth', AuthCOntroller);
+
 module.exports = app;
